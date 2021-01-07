29a8530936a45a8198abcef13b52eac9430cfc1e
AlAlSanaraAndroid <alal.android@sanara.ru>
2021-01-07 23:08:34 +0400

    с телефона, фикс урла

--- main.js
+++ main.js
@@ -74,7 +74,7 @@
     editor.setOption("fullScreen", true);
     a = document.getElementById('src');
     url = '/trash/i3_formatted.json (1)';
-    url = '/news (2).json';
+    url = '/trash/news (2).json';
     a.href = url;
     a.innerHTML = url;
     fetch(url)
