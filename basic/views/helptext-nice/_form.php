e0accd3a773b66dc342ed215a9cc4e4809597939
alal <alal@notebook>
2021-01-11 21:54:55 +0400

    TiniMCE  но он не  отображает переносы строк в  полях  справки !

--- basic/views/helptext-nice/_form.php
+++ basic/views/helptext-nice/_form.php
@@ -1,11 +1,24 @@
 <?php
 
+use dosamigos\tinymce\TinyMce;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Helptext */
 /* @var $form yii\widgets\ActiveForm */
+$default = [
+    'options' => ['rows' => 16],
+    'language' => 'en_GB',
+    'clientOptions' => [
+        'plugins' => [
+            "advlist autolink lists link charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste"
+        ],
+        'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    ]
+];
 ?>
 
 <div class="helptext-form">
@@ -18,19 +31,19 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'updated')->textInput() ?>
 
-    <?= $form->field($model, 'help')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'help')->widget(TinyMce::className(), $default) ?>
 
-    <?= $form->field($model, 'decr')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'decr')->widget(TinyMce::className(), $default) ?>
 
-    <?= $form->field($model, 'example')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'example')->widget(TinyMce::className(), $default) ?>
 
-    <?= $form->field($model, 'parsed')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'parsed')->widget(TinyMce::className(), $default) ?>
 
-    <?= $form->field($model, 'source')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'source')->widget(TinyMce::className(), $default) ?>
 
-    <?= $form->field($model, 'device')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'device')->widget(TinyMce::className(), $default) ?>
 
-    <?= $form->field($model, 'dop_info')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'dop_info')->widget(TinyMce::className(), $default) ?>
 
     <?= $form->field($model, 'weight')->textInput() ?>
 
